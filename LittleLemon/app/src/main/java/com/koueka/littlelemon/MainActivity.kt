df841083a511e23ca27cd52bdd7eee0133b515da
Karly KOUEKA <karlykoueka@hotmail.com>
2025-03-23 00:48:15 -0400

    Implement Onbording page
    Base on the details provided for this exercice
    and base on the provided mock from Meta,
    This page has been implemented accordingly
    
    It could be great to revisit this and implement
    unittest if possible. It is imprtant to check
    the colors, the text font and style match the
    expected result.

--- LittleLemon/app/src/main/java/com/koueka/littlelemon/MainActivity.kt
+++ LittleLemon/app/src/main/java/com/koueka/littlelemon/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.koueka.littlelemon.composable.Onboarding
+//import com.koueka.littlelemon.composable.Onboarding
 import com.koueka.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,12 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             LittleLemonTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    Onboarding()
-                    /*Greeting(
+                    //Onboarding()
+                    Greeting(
                         name = "Karly",
                         modifier = Modifier.padding(innerPadding)
-                    )*/
+                    )
                 }
             }
         }
