fe208da9a101d169817103c76c99939d6999b244
Karly KOUEKA <karlykoueka@hotmail.com>
2025-03-23 17:29:35 -0400

    Profile setup - logo
    profile screen has been updated with the
    brand logo
    
    next step complete the design of the page.

--- LittleLemon/app/src/main/java/com/koueka/littlelemon/composable/Profile.kt
+++ LittleLemon/app/src/main/java/com/koueka/littlelemon/composable/Profile.kt
@@ -1,18 +1,22 @@
 package com.koueka.littlelemon.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.koueka.littlelemon.R
 import com.koueka.littlelemon.ui.theme.Green80
 import com.koueka.littlelemon.ui.theme.LittleLemonTheme
 
@@ -20,6 +24,15 @@ import com.koueka.littlelemon.ui.theme.LittleLemonTheme
 @Composable
 fun Profile() {
     Column(modifier = Modifier.padding(top = 30.dp, start = 0.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .padding(50.dp)
+                .align(Alignment.CenterHorizontally)
+
+        )
+
         Text(
             text = "Profile",
             modifier = Modifier
