f3a790053af4569336ae4092fd3497d84b8f2750
Karly KOUEKA <karlykoueka@hotmail.com>
2025-03-24 21:17:25 -0400

    Save and Display MenuItem
    the second part of dispaying the menu item
    is completed. Some UI composable have also
    been updated.
    
    - Added livedata lib
    - Added glide compose lib
    - Updated library catalog
    - Updated gradle build file
    - Built database management
    - Saved menu Item in the Room database
    - Added new image for Hero section
    - Added Karla and Markazi font
    
    Those are the changes included with this
    commit.

--- LittleLemon/app/src/main/java/com/koueka/littlelemon/ui/theme/Type.kt
+++ LittleLemon/app/src/main/java/com/koueka/littlelemon/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.koueka.littlelemon.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.koueka.littlelemon.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,12 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+var karlaFamily = FontFamily(
+    Font(R.font.karla_regular, FontWeight.Normal)
+)
+
+var markazitextFamily = FontFamily(
+    Font(R.font.markazitext_regular, FontWeight.Normal)
 )
